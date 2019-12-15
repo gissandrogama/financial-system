a579652bb73d8ef5dfa47c36c0b9beafd0486f80
gissandrogama <gissandrogama@gmail.com>
2019-12-15 17:55:05 -0300

    add lib Money and modification module Account

--- lib/account.ex
+++ lib/account.ex
@@ -1,31 +1,35 @@
 defmodule FinancialSystem.Account do
+
+  alias Money
+
   @moduledoc """
-  Account structure module and function to create an account.
+  Account structure module and function to create an account, using the lib Money.
   """
-
-
-  # @enforce_keys [:name, :email]
-  defstruct name: "", email: "", currency: "BRL", balance: 0
+  
+  @enforce_keys [:name, :email]
+  defstruct [name: nil , email: nil, balance: Money.new(0, :BRL)]
 
   alias __MODULE__
+
+  @typedoc """
+
+  """
+  @type t :: %FinancialSystem.Account{name: String.t(), email: String.t(), balance: Money.t()}
 
   @doc """
   Function for create a new account.
 
   ##Exemples
-    iex(2)> account1 = Account.new(name: "Henry", email: "henrygama@gmail.com", currency: "BRL")
-    %FinancialSystem.Account{
-      balance: 0,
-      currency: "BRL",
-      email: "henrygama@gmail.com",
-      name: "Henry"
+  account1 = FinancialSystem.Account.new("Gissandro","gissandrogama@gmail.com", 100, :BRL)
+  %FinancialSystem.Account{
+    balance: %Money{amount: 100, currency: :BRL},
+    email: "gissandrogama@gmail.com",
+    name: "Gissandro"
   }
   """
-
-  @spec new([{:currency, any} | {:email, any} | {:name, any}, ...]) :: FinancialSystem.Account.t()
-  def new(name: name, email: email, currency: currency) do
-    %Account{name: name, email: email, currency: currency}
+  @spec new(String.t(), String.t(), integer, atom) :: FinancialSystem.Account.t()
+  def new(name, email, amount, currency) do
+    %Account{name: name, email: email, balance: Money.new(amount, currency)}
   end
 
 end
-
