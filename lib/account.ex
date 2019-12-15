14e7f5ce177f7d769a9e95fc87d13c9b23ea4eb5
gissandrogama <gissandrogama@gmail.com>
2019-12-15 18:28:37 -0300

    @typedoc  landing

--- lib/account.ex
+++ lib/account.ex
@@ -5,14 +5,14 @@ defmodule FinancialSystem.Account do
   @moduledoc """
   Account structure module and function to create an account, using the lib Money.
   """
-  
+
   @enforce_keys [:name, :email]
   defstruct [name: nil , email: nil, balance: Money.new(0, :BRL)]
 
   alias __MODULE__
 
   @typedoc """
-
+  Type that makes the specifications of the most descriptive function.
   """
   @type t :: %FinancialSystem.Account{name: String.t(), email: String.t(), balance: Money.t()}
 
