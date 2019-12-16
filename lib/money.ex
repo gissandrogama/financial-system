f19029b13bef3db77c6a6fa52eb567b4208001c0
gissandrogama <gissandrogama@gmail.com>
2019-12-16 16:01:54 -0300

    documentation description

--- lib/money.ex
+++ lib/money.ex
@@ -1,5 +1,10 @@
 defmodule FinancialSystem.Money do
     @moduledoc """
+    Module that solves floating point arithmetic problems using decimal. 
+    Currency Related Structure and Function.
+    """
+
+    @doc """
     
     """
 end
