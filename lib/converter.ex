54b4a9e94a853e81238a2b9e2ff2992c81eeae72
gissandrogama <gissandrogama@gmail.com>
2019-12-17 17:46:51 -0300

    function exchange

--- lib/converter.ex
+++ lib/converter.ex
@@ -1,3 +1,17 @@
 defmodule FinancialSystem.Converter do
     
+    alias FinancialSystem.Currency, as: Currency
+
+    @moduledoc """
+    Module that treats operations such as currency conversion.
+    """
+
+    @doc """
+    
+    """
+    def to_exchange() do
+        
+    end
+
+
 end
