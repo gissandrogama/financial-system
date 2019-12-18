9f506c5ba6be8ba728c20f1280bf416a986ee910
gissandrogama <gissandrogama@gmail.com>
2019-12-18 02:23:42 -0300

    functions for coverter coin

--- lib/converter.ex
+++ lib/converter.ex
@@ -5,23 +5,21 @@ defmodule FinancialSystem.Converter do
   Module that treats operations such as currency conversion.
   """
 
-  @doc """
 
-  """
-  def exchenge(amount, from, to) when from == to do
-    case Currency.is_valid?(from) do
+  @doc false
+  def exchange(amount, from_coin, to_coin) when from_coin == to_coin do
+    case Currency.is_valid?(from_coin) do
       true -> {:ok, amount}
-      false -> {:error, "Coin (#{from}) not valid compared to ISO 4271"}
+      false -> {:error, "Coin (#{from_coin}) not valid compared to ISO 4271"}
     end
   end
 
-  @spec exchenge(any, :USD, atom) :: {:error, <<_::64, _::_*8>>} | {:ok, float}
-  def exchenge(amount, :USD, to) do
+  def exchange(amount, :USD, to) do
     case Currency.is_valid?(to) do
       true ->
         rates = FinancialSystem.Currency.parse("currency_rates.txt")
-        value_amount = Decimal.new(amount)
-        rate_to = Decimal.new(rates[to])
+        value_amount = Decimal.from_float(amount)
+        rate_to = Decimal.from_float(rates[to])
 
         converted_amount =
           Decimal.mult(value_amount, rate_to)
@@ -35,12 +33,12 @@ defmodule FinancialSystem.Converter do
     end
   end
 
-  def exchenge(amount, from, :USD) do
+  def exchange(amount, from, :USD) do
     case Currency.is_valid?(from) do
       true ->
         rates = FinancialSystem.Currency.parse("currency_rates.txt")
-        value_amount = Decimal.new(amount)
-        rate_from = Decimal.new(rates[from])
+        value_amount = Decimal.from_float(amount)
+        rate_from = Decimal.from_float(rates[from])
 
         converted_amount =
           Decimal.mult(value_amount, rate_from)
@@ -59,11 +57,18 @@ defmodule FinancialSystem.Converter do
   """
   @spec exchange(float, atom, atom) :: {:ok, float} | {:error, String.t()}
   def exchange(amount, from, to) do
-    usd_value = FinancialSystem.Currency.exchange!(amount, from, :USD)
-    FinancialSystem.Currency.exchange!(usd_value, :USD, to)
+    usd_value = FinancialSystem.Converter.exchange!(amount, from, :USD)
+    FinancialSystem.Converter.exchange!(usd_value, :USD, to)
   end
 
-  
+  @doc """
 
-
+  """
+  @spec exchange!(float, atom, atom) :: float | no_return
+  def exchange!(amount, from_coin, to_coin) do
+    case exchange(amount, from_coin, to_coin) do
+      {:ok, result} -> result
+      {:error, reason} -> raise(reason)
+    end
+  end
 end
