aadc554deb1ccdcc4c6c2088de4bbb4bcff53a44
gissandrogama <gissandrogama@gmail.com>
2019-12-23 00:12:07 -0300

    function of tranfer

--- lib/financial_system.ex
+++ lib/financial_system.ex
@@ -7,6 +7,26 @@ defmodule FinancialSystem do
   """
 
   @doc """
+
+  ##Examples
+
+  """
+
+  @spec translation(Account.t(), Account.t() | [Account.t()], integer) ::
+    {Account.t(), Account.t() | [Account.t()]} | {:error, String.t()}
+  def translation(from_account, to_account, value) do
+    case balance_enough?(from_account, value) do
+      true ->
+        split_value =
+        value / length(to_account)
+        |> Decimal.from_float()
+        |> Decimal.round()
+      false ->
+        {:error, "Not enough money. (balance #{from_account.balance.amount})"}
+    end
+  end
+
+  @doc """
   Funciotion to debit of a value in a account specifies
 
   ## Examples
@@ -14,14 +34,14 @@ defmodule FinancialSystem do
 
   @spec debit(Account.t(), Money.t(), atom, integer) :: Account.t() | {:error, String.t()}
   def debit(account, money, key, value) do
-    case balance_enough(money, value) do
+    case balance_enough?(money, value) do
       true ->
         current = Money.subtract(money, value)
         up_account(account, key, current)
 
       false ->
         {:error, "Not enough money. (balance #{money.amount})"}
-      end
+    end
   end
 
   @doc """
@@ -32,8 +52,8 @@ defmodule FinancialSystem do
 
   @spec deposit(Account.t(), Money.t(), atom, integer) :: Account.t()
   def deposit(account, money, key, value) do
-        current = Money.add(money, value)
-        up_account(account, key, current)
+    current = Money.add(money, value)
+    up_account(account, key, current)
   end
 
   @spec up_account(Account.t(), atom, Money.t()) :: Account.t()
@@ -41,8 +61,8 @@ defmodule FinancialSystem do
     Map.put(account, key, balance_current)
   end
 
-  @spec balance_enough(Money.t(), integer) :: boolean
-  def balance_enough(balance, value) do
+  @spec balance_enough?(Money.t(), integer) :: boolean
+  def balance_enough?(balance, value) do
     balance.amount >= value
   end
 end
